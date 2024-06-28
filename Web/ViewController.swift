176bbbf45d7738530a89dc6e49e74a3ead9db576
NewSouthWave <tigers0110@naver.com>
2024-06-28 12:41:37 +0900

    Initial Commit

---
+++ Web/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web
+//
+//  Created by Nam on 2024/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
